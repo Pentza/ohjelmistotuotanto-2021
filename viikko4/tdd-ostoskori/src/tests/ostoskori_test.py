d4edbb7e8148d1503887a8c40a287a1ec0bb4dc6
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-11-29 18:05:23 +0200

    lisää testi: tyhjenna(), tyhjentää ostoskorin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -119,3 +119,14 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(len(self.kori.ostokset()), 0)
+
+    def test_tyhjenna_tyhjentaa_korin_jossa_tuotteita(self):
+        maito = Tuote("Maito", 3)
+        kahvi = Tuote("kahvi", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kahvi)
+        self.kori.lisaa_tuote(kahvi)
+        self.kori.tyhjenna()
+
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(len(self.kori.ostokset()), 0)
